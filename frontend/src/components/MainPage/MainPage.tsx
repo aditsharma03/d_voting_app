c2975f8c0bdef815505521d12e6aea7b72e78dbf
Adit <aditsharma1203@gmail.com>
2025-03-21 18:16:39 +0530

    testing in deployment

--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -5,21 +5,22 @@ import { WalletContext } from "../../contexts/WalletContext";
 import { ethers } from "ethers";
 
 
-import deployedaddresses from "../../artifacts/deployed_addresses.json";
+import deployedaddresses from "../../artifacts/contract-address.json";
 import {abi} from "../../artifacts/VotingApplication.json";
 
 
 
 const MainPage = () => {
 
-  const {signer} = useContext(WalletContext);
+  const {signer, signerAddress} = useContext(WalletContext);
   const [pollCount, setPollCount] = useState(9999);
+  const [myPolls, setMyPolls] = useState();
    
 
 
   const VotingApplication = useMemo(() => {
     return new ethers.Contract(
-      deployedaddresses["VotingApplicationModule#VotingApplication"],
+      deployedaddresses["VotingApplication"],
       abi,
       signer,
     );
@@ -32,11 +33,46 @@ const MainPage = () => {
       const getPollCount = async () => {
         const _pollCount = await VotingApplication.pollCount();
         setPollCount(_pollCount);
+
+        //const _myPolls = await VotingApplication.owners(signerAddress);
+        //setMyPolls(_polls);
+        
+        
+    const candidateobj1 = {
+        id: "1",
+        name: "aniketneg",
+        description: "bublu",
+    };
+    const candidateobj2 = {
+        id: "2",
+        name: "kothiyal",
+        description: "abhishek",
+    };
+
+    const candidates = [candidateobj1, candidateobj2];
+
+    const pollobj1 = {
+        id: "uuid1"+(new Date).getTime(),
+        realTimeTally: true,
+        isStartAutomated: false,
+        isEndAutomated: false,
+        startTime: 0,
+        endTime: 0,
+        candidates: candidates,
+        eligibleVoters: [signerAddress]
+    }
+        
+        
+        const txn = await VotingApplication.createPoll(pollobj1);
+       setMyPolls(txn);
+        
+        
+        
       };
       getPollCount();
-    }, 10000);
+    }, 5000);
     return ()=>clearInterval(x)
-  }, [signer, VotingApplication]);
+  }, [signer, signerAddress, VotingApplication]);
 
 
 
@@ -46,6 +82,8 @@ const MainPage = () => {
 
         <div className="h-full w-full flex flex-col items-center">
             <WalletStatus />
+      {pollCount}
+      {JSON.stringify(myPolls)}
             <Outlet />
         </div>
     );
